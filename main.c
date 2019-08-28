a0973c3f0cc35ad3c28d61d45573de38edc742d1
marie-nde <marie9nde@gmail.com>
2019-08-28 15:25:14 +0200

    maj du main, 6th commit

--- main.c
+++ main.c
@@ -50,4 +50,45 @@ int		main(int ac, char **av)
 		write(1, "Your result : ", 14);
 		printf("%d\n\n", ft_isalnum(atoi(av[1])));
 	}
+	if (ac == 3)
+	{
+		char *cpy = ft_strdup(av[1]);
+		char *cpy2 = ft_strdup(av[2]);
+		char *cpy3 = ft_strdup(av[1]);
+		char *cpy4 = ft_strdup(av[2]);
+		char *cat = ft_strdup(av[1]);
+		char *cat2 = ft_strdup(av[2]);
+		char *cat3 = ft_strdup(av[1]);
+		char *cat4 = ft_strdup(av[2]);
+
+		write(1, "### ft_strcmp ###\n", 18);
+		write(1, "Test with the first and second argument.\n", 41);
+		write(1, "Result expected : ", 18);
+		printf("%d\n", strcmp(av[1], av[2]));
+		write(1, "Your result : ", 14);
+		printf("%d\n\n", ft_strcmp(av[1], av[2]));
+
+		write(1, "### ft_strcpy ###\n", 18);
+		write(1, "Test with the first and second argument.\n", 41);
+		write(1, "Result expected : ", 18);
+		printf("%s\n", strcpy(cpy3, cpy4));
+		write(1, "Your result : ", 14);
+		printf("%s\n\n", ft_strcpy(cpy, cpy2));
+
+		write(1, "### ft_strcat ###\n", 18);
+		write(1, "Test with the first and second argument.\n", 41);
+		write(1, "Result expected : ", 18);
+		printf("%s\n", strcat(cat, cat2));
+		write(1, "Your result : ", 14);
+		printf("%s\n\n", ft_strcat(cat3, cat4));
+	}
+	if (ac == 4)
+	{
+		write(1, "### ft_strncmp ###\n", 19);
+		write(1, "Test with the first, second and third argument.\n", 48);
+		write(1, "Result expected : ", 18);
+		printf("%d\n", strncmp(av[1], av[2], atoi(av[3])));
+		write(1, "Your result : ", 14);
+		printf("%d\n\n", ft_strncmp(av[1], av[2], atoi(av[3])));
+	}
 }
